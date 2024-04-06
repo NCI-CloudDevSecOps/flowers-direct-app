965789b064524bbab0420c89205c7915cc624a4a
EC2 Default User <ec2-user@ip-172-31-42-98.eu-west-1.compute.internal>
2024-04-06 01:28:53 +0000

    css

--- flowersdirect/settings.py
+++ flowersdirect/settings.py
@@ -147,6 +147,7 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 SITE_ID = 1
